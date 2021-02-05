111272758035ce2b258708a8ab3f3d5e07d22b71
Alex van Vliet <alex@vanvliet.pro>
2021-02-05 16:53:56 +0100

    feat: add length in string field

--- src/Fields/StringField.php
+++ src/Fields/StringField.php
@@ -6,8 +6,14 @@ namespace AlexVanVliet\Migratify\Fields;
 
 class StringField extends Field
 {
+    public function __construct(
+        protected int $length = 255,
+    )
+    {
+    }
+
     public function create(string $name): string
     {
-        return "\$table->string('$name');";
+        return "\$table->string('$name', {$this->length});";
     }
 }
